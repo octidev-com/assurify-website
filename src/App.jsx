800f825687aa07473fab61158b0b9399eef0de05
Mahmudul Haque Noor <mahmudulhaquenoor@gmail.com>
2025-04-28 20:00:05 +0600

    feat: pricing page design complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import Navbar from './component/Navbar'
 import Footer from './component/Footer'
 import Home from './pages/Home'
 import { Route, Routes } from 'react-router'
+import Pricing from './pages/Pricing'
 
 const App = () => {
   return (
-    <div className='w-full mx-auto bg-[#0C0D0C] min-h-screen'>
+    <div className='w-full mx-auto bg-[#0C0D0C] text-[#fff] min-h-screen'>
       <div className='fixed top-0 left-0 w-full z-[100]'>
         <Navbar />
       </div>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/pricing' element={<Pricing />} />
       </Routes>
       <Footer />
     </div>
--- src/component/Navbar/index.jsx
+++ src/component/Navbar/index.jsx
@@ -1,45 +1,66 @@
 import React from 'react'
 import OrgIcon from '../../assets/icon/assurify-icon.png'
+import { NavLink } from 'react-router'
 
 const Navbar = () => {
   return (
     <div className='bg-[rgba(255,255,255,0.12)] backdrop-blur-[37px] border-b-[0.5px] border-b-[rgba(255,255,255,0.6)] text-white flex justify-center w-full items-center p-6 sticky top-0 z-50'>
       <div className='container flex justify-between items-center'>
-        <img src={OrgIcon} alt='' />
-        <div className='flex gap-6 '>
-          <a
-            className='text-[16px] font-normal active:text-[#48BD42] hover:text-[#48BD42]'
-            href=''>
+        <img src={OrgIcon} alt='Assurify logo' className='w-[100px] h-[25px]' />
+        <div className='flex gap-6'>
+          <NavLink
+            to='/'
+            className={({ isActive }) =>
+              `text-[16px] font-normal ${
+                isActive ? 'text-[#48BD42]' : 'text-white'
+              } hover:text-[#48BD42]`
+            }>
             Home
-          </a>
-          <a
-            className='text-[16px] font-normal active:text-[#48BD42] hover:text-[#48BD42]'
-            href=''>
+          </NavLink>
+          <NavLink
+            to='/documentation'
+            className={({ isActive }) =>
+              `text-[16px] font-normal ${
+                isActive ? 'text-[#48BD42]' : 'text-white'
+              } hover:text-[#48BD42]`
+            }>
             Documentation
-          </a>
-          <a
-            className='text-[16px] font-normal active:text-[#48BD42] hover:text-[#48BD42]'
-            href=''>
+          </NavLink>
+          <NavLink
+            to='/discussion'
+            className={({ isActive }) =>
+              `text-[16px] font-normal ${
+                isActive ? 'text-[#48BD42]' : 'text-white'
+              } hover:text-[#48BD42]`
+            }>
             Discussion
-          </a>
-          <a
-            className='text-[16px] font-normal active:text-[#48BD42] hover:text-[#48BD42]'
-            href=''>
+          </NavLink>
+          <NavLink
+            to='/deals'
+            className={({ isActive }) =>
+              `text-[16px] font-normal ${
+                isActive ? 'text-[#48BD42]' : 'text-white'
+              } hover:text-[#48BD42]`
+            }>
             Deals
-          </a>
-          <a
-            className='text-[16px] font-normal active:text-[#48BD42] hover:text-[#48BD42]'
-            href=''>
+          </NavLink>
+          <NavLink
+            to='/pricing'
+            className={({ isActive }) =>
+              `text-[16px] font-normal ${
+                isActive ? 'text-[#48BD42]' : 'text-white'
+              } hover:text-[#48BD42]`
+            }>
             Pricing
-          </a>
+          </NavLink>
         </div>
         <div className='flex gap-[30px]'>
-          <button type='button' className='btn-secondary'>
+          <NavLink to='/demo' className='btn-secondary'>
             View Demo
-          </button>
-          <button type='button' className='btn-primary'>
+          </NavLink>
+          <NavLink to='/pricing' className='btn-primary'>
             Try For Free
-          </button>
+          </NavLink>
         </div>
       </div>
     </div>
