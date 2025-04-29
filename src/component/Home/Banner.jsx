b9984a8517e0ebeee8ab97e39ec0316d25ea9539
Mahmudul Haque Noor <mahmudulhaquenoor@gmail.com>
2025-04-29 20:28:43 +0600

    chore: a simple change made in home page banner component

--- src/component/Home/Banner.jsx
+++ src/component/Home/Banner.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
+import Container from '../Common/Container'
 
 const Banner = () => {
   return (
-    <div className='container flex justify-center mt-6 sm:mt-8 lg:mt-10'>
-      <div className='max-w-[90%] sm:max-w-[500px] lg:max-w-[600px] text-center'>
-        <h1 className='text-[32px] sm:text-[56px] lg:text-[96px] font-semibold leading-[38px] sm:leading-[64px] lg:leading-[110px] mb-2 sm:mb-3 lg:mb-4'>
-          Guard Your Packages
-        </h1>
-        <p className='text-sm sm:text-base lg:text-lg font-[300] text-[#F7F7F7] mb-4 sm:mb-5 lg:mb-6'>
-          Assurify isn't just about protecting lost, stolen, or damaged
-          packages; it's about protecting your brand.
-        </p>
-        <div className='flex justify-center gap-2 sm:gap-4 lg:gap-5'>
-          <button className='btn-primary text-xs sm:text-sm lg:text-base px-3 sm:px-4 lg:px-5 py-1 sm:py-2 lg:py-2'>
-            How it works
-          </button>
-          <button className='btn-secondary text-xs sm:text-sm lg:text-base px-3 sm:px-4 lg:px-5 py-1 sm:py-2 lg:py-2'>
-            View Demo
-          </button>
+    <div className='mt-[42px] md:mt-14'>
+      <Container>
+        <div className='max-w-[90%] sm:max-w-[500px] lg:max-w-[600px] text-center mx-auto'>
+          <h1 className='text-[32px] sm:text-[56px] lg:text-[96px] font-semibold leading-[38px] sm:leading-[64px] lg:leading-[110px] mb-2 sm:mb-3 lg:mb-4'>
+            Guard Your Packages
+          </h1>
+          <p className='text-sm sm:text-base lg:text-lg font-[300] text-[#F7F7F7] mb-4 sm:mb-5 lg:mb-6'>
+            Assurify isn't just about protecting lost, stolen, or damaged
+            packages; it's about protecting your brand.
+          </p>
+          <div className='flex justify-center gap-2 sm:gap-4 lg:gap-5'>
+            <button className='btn-primary text-xs sm:text-sm lg:text-base px-3 sm:px-4 lg:px-5 py-1 sm:py-2 lg:py-2'>
+              How it works
+            </button>
+            <button className='btn-secondary text-xs sm:text-sm lg:text-base px-3 sm:px-4 lg:px-5 py-1 sm:py-2 lg:py-2'>
+              View Demo
+            </button>
+          </div>
         </div>
-      </div>
+      </Container>
     </div>
   )
 }
