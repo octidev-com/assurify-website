8afc83c2b4775e93a96e9c6866c0807656f8fccf
Mahmudul Haque Noor <mahmudulhaquenoor@gmail.com>
2025-04-30 12:33:16 +0600

    refactor: remove width class

--- src/component/Common/SectionTitle.jsx
+++ src/component/Common/SectionTitle.jsx
@@ -1,24 +1,38 @@
-import rightSideHr from "../../assets/howItWorks/right-side-hr.png";
-import leftSideHr from "../../assets/howItWorks/left-side-hr.png";
+import rightSideHr from '../../assets/howItWorks/right-side-hr.png'
+import leftSideHr from '../../assets/howItWorks/left-side-hr.png'
 
 const SectionTitle = ({ topText, middleText, bottomText }) => {
   return (
-    <div className="w-[732px] max-w-full mx-auto flex flex-col gap-2 mt-[32px] lg:mt-[56px]">
+    <div className='mx-auto flex flex-col gap-2 mt-[32px] lg:mt-[56px]'>
       {topText && (
-        <div className="flex justify-center items-center gap-3">
-          <img src={rightSideHr} alt="Right horizontal line" className="max-w-[100px]" />
-          <p className="text-[14px] lg:text-xl font-normal leading-[21px] lg:leading-[30px]">{topText}</p>
-          <img src={leftSideHr} alt="Left horizontal line" className="max-w-[100px]" />
+        <div className='flex justify-center items-center gap-3'>
+          <img
+            src={rightSideHr}
+            alt='Right horizontal line'
+            className='max-w-[100px]'
+          />
+          <p className='text-[14px] lg:text-xl font-normal leading-[21px] lg:leading-[30px]'>
+            {topText}
+          </p>
+          <img
+            src={leftSideHr}
+            alt='Left horizontal line'
+            className='max-w-[100px]'
+          />
         </div>
       )}
       {middleText && (
-        <h1 className="text-[24px] lg:text-5xl font-semibold text-gradient text-center leading-[36px] lg:leading-[72px]">{middleText}</h1>
+        <h1 className='text-[24px] lg:text-5xl font-semibold text-gradient text-center leading-[36px] lg:leading-[72px]'>
+          {middleText}
+        </h1>
       )}
       {bottomText && (
-        <p className="text-[#A6A6A6] text-[12px] lg:text-lg font-light text-center leading-[18px] lg:leading-[27px]">{bottomText}</p>
+        <p className='text-[#A6A6A6] text-[12px] lg:text-lg font-light text-center leading-[18px] lg:leading-[27px]'>
+          {bottomText}
+        </p>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default SectionTitle;
+export default SectionTitle
