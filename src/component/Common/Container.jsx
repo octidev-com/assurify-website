556db5383a5ad6aefb9f7b3bb18ecd07da8d01ff
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2025-04-30 10:22:30 +0600

    feat: add deals page responsive layout

--- src/component/Common/Container.jsx
+++ src/component/Common/Container.jsx
@@ -1,5 +1,5 @@
 const Container = ({ children }) => {
-  return <div className="max-w-[1170px] mx-auto px-[16px] lg:px-0">{children}</div>;
+  return <div className="max-w-[1170px] mx-auto px-[24px] xl:px-0">{children}</div>;
 };
 
 export default Container;
