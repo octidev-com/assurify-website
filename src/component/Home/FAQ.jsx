1c3bd6fa6fd0b0254d0d38d3139dad959f549a28
Mahmudul Haque Noor <mahmudulhaquenoor@gmail.com>
2025-04-30 13:38:58 +0600

    refactor: faq component responsive design complete

--- src/component/Home/FAQ.jsx
+++ src/component/Home/FAQ.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
-import rightSideHr from '../../assets/howItWorks/right-side-hr.png'
-import leftSideHr from '../../assets/howItWorks/left-side-hr.png'
 import CustomIcon from '../../assets/icon/CustomIcon'
+import SectionTitle from '../Common/SectionTitle'
+import Container from '../Common/Container'
 
 const FAQ = () => {
   const [openFAQ, setOpenFAQ] = useState(null)
@@ -56,57 +56,53 @@ const FAQ = () => {
   }
 
   return (
-    <div className='container mt-[150px]'>
-      {/* Header Section */}
-      <div className='w-[732px] max-w-full mx-auto flex flex-col gap-4 px-4'>
-        <div className='flex justify-center items-center gap-3'>
-          <img src={rightSideHr} alt='' className='max-w-[100px]' />
-          <p className='text-xl font-normal leading-[30px]'>Customer Support</p>
-          <img src={leftSideHr} alt='' className='max-w-[100px]' />
-        </div>
-        <h1 className='text-5xl font-semibold text-gradient text-center leading-[72px]'>
-          Frequently Asked Question
-        </h1>
-        <p className='text-[#A6A6A6] text-lg font-light text-center leading-[27px]'>
-          Find answers to common questions and get the support you need—quick
-          and easy
-        </p>
-      </div>
+    <Container>
+      <div className='mt-[150px]'>
+        {/* Header Section */}
 
-      {/* FAQ Section */}
-      <div className='max-w-[732px] mx-auto mt-12'>
-        {faqs.map((faq) => (
-          <div
-            key={faq.id}
-            className='bg-gradient-to-r from-white/5 to-white/5 backdrop-blur-[37px] rounded-2xl mb-4 overflow-hidden'>
-            <button
-              className='flex items-center gap-4 p-4 w-full text-left'
-              onClick={() => toggleFAQ(faq.id)}>
-              <CustomIcon
-                iconName={
-                  openFAQ === faq.id
-                    ? 'accordion-close-icon'
-                    : 'accordion-open-icon'
-                }
-              />
-              <p className='text-lg font-normal text-[#f7f7f7]'>
-                {faq.question}
-              </p>
-            </button>
+        <SectionTitle
+          topText={'Customer Support'}
+          middleText={'Frequently Asked Question'}
+          bottomText={
+            'Find answers to common questions and get the support you need—quick and easy'
+          }
+        />
+
+        {/* FAQ Section */}
+        <div className='max-w-[732px] mx-auto mt-12'>
+          {faqs.map((faq) => (
             <div
-              className={`transition-all duration-300 ease-in-out ${
-                openFAQ === faq.id
-                  ? 'max-h-40 opacity-100'
-                  : 'max-h-0 opacity-0'
-              } overflow-hidden`}>
-              <p className='p-4 pt-0 text-[#A6A6A6] text-base font-light leading-6'>
-                {faq.answer}
-              </p>
+              key={faq.id}
+              className='bg-gradient-to-r from-white/5 to-white/5 backdrop-blur-[37px] rounded-2xl mb-4 overflow-hidden'>
+              <button
+                className='flex items-center gap-4 p-4 w-full text-left'
+                onClick={() => toggleFAQ(faq.id)}>
+                <CustomIcon
+                  iconName={
+                    openFAQ === faq.id
+                      ? 'accordion-close-icon'
+                      : 'accordion-open-icon'
+                  }
+                />
+                <p className='text-lg font-normal text-[#f7f7f7]'>
+                  {faq.question}
+                </p>
+              </button>
+              <div
+                className={`transition-all duration-300 ease-in-out ${
+                  openFAQ === faq.id
+                    ? 'max-h-40 opacity-100'
+                    : 'max-h-0 opacity-0'
+                } overflow-hidden`}>
+                <p className='p-4 pt-0 text-[#A6A6A6] text-base font-light leading-6'>
+                  {faq.answer}
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </Container>
   )
 }
 
