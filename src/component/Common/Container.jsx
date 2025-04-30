7d5c29ca9fa7d07a5db59192c48f2a21a4fe0bc5
rahatul99 <rahatul99@gmail.com>
2025-04-30 20:01:15 +0600

    refactor[container, for-merchant]: updated the container and for merchant component

--- src/component/Common/Container.jsx
+++ src/component/Common/Container.jsx
@@ -1,5 +1,24 @@
-const Container = ({ children }) => {
-  return <div className="max-w-[1170px] mx-auto px-[24px] xl:px-0">{children}</div>;
+const Container = ({
+  children,
+  verticalPadding,
+  paddingTop,
+  horizontalPadding = "24px",
+}) => {
+  const style = {
+    paddingTop: paddingTop ? paddingTop : undefined,
+    paddingBottom: verticalPadding ? verticalPadding : undefined,
+    paddingLeft: horizontalPadding ? horizontalPadding : undefined,
+    paddingRight: horizontalPadding ? horizontalPadding : undefined,
+  };
+  return (
+    <div
+      className="max-w-[1170px] mx-auto xl:px-0"
+      style={style}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Container;
+
