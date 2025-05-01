b25aed2f17c437cf6fd855467c5e27f613de6b13
rahatul99 <rahatul99@gmail.com>
2025-05-01 13:43:41 +0600

    refactor[sectionTitle, forMerchant]: fix the style issues for mobile

--- src/component/ForMerchant/ForMerchantSection.jsx
+++ src/component/ForMerchant/ForMerchantSection.jsx
@@ -3,7 +3,6 @@ import writingIcon from "../../assets/support/writing.svg";
 import clockIcon from "../../assets/support/clock.svg";
 import supportIcon from "../../assets/support/support.svg";
 import supportImage from "../../assets/support/supportImage.svg";
-import bannerBg from "../../assets/banner/banner-bg.png";
 import rightSideHr from "../../assets/howItWorks/right-side-hr.png";
 import leftSideHr from "../../assets/howItWorks/left-side-hr.png";
 import benefit1 from "../../assets/benefits/benefit-1.png";
@@ -41,7 +40,7 @@ const ForMerchantSection = () => {
   return (
     <>
       {/* Hero Section */}
-      <div className="w-full px-[24px] bg-cover bg-no-repeat bg-center z-10 relative pt-[56px] pb-[150px]">
+      <div className="w-full px-[24px] bg-cover bg-no-repeat bg-center z-10 relative sm:pt-[56px] pt-[32px] pb-[50px] sm:pb-[150px]">
         <Container horizontalPadding="0">
           <SectionTitle
             topText="For Merchant"
@@ -49,25 +48,17 @@ const ForMerchantSection = () => {
             bottomText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mollis ante eu sem tempus, eleifend sollicitudin felis cursus.Praesent non ante vitae turpis."
           />
           <div className="flex justify-center gap-7 pt-[30px]">
-            <Button
-              buttonText="How it works"
-              variant="primary-white"
-              className="rounded-full px-6 py-3 hover:bg-gray-200 transition"
-            />
+            <Button buttonText="How it works" variant="primary-white" />
 
-            <Button
-              buttonText="View Demo"
-              variant="secondary-outline"
-              className="rounded-full px-6 py-3 hover:bg-white hover:text-black transition"
-            />
+            <Button buttonText="View Demo" variant="secondary-outline" />
           </div>
         </Container>
-        <div className="max-w-[1170px] mx-auto bg-[#EDEDED] w-full h-[812px] rounded-lg shadow-md mt-12 flex justify-center items-center">
+        <div className="max-w-[1170px] mx-auto bg-[#EDEDED] w-full max-h-[812px] rounded-lg shadow-md mt-12 flex justify-center items-center">
           <p className="text-[#888] text-lg">Image Goes Here</p>
         </div>
       </div>
       {/* Brands Section */}
-      <div className="w-full py-[24px] px-[24px] lg:py-[94px] relative bg-gradient-to-r from-white/5 to-white/5 backdrop-blur-[37px] overflow-hidden">
+      <div className="w-full py-[24px] px-[0] sm:px-[24px] lg:py-[94px] relative bg-gradient-to-r from-white/5 to-white/5 backdrop-blur-[37px] overflow-hidden">
         <div className="absolute top-1/2 left-[-50%] transform -translate-y-1/2 pointer-events-none z-0">
           <img src={roundBg} alt="Background decoration" />
         </div>
@@ -81,47 +72,52 @@ const ForMerchantSection = () => {
             bottomText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mollis ante eu sem tempus, eleifend sollicitudin felis cursus.Praesent non ante vitae turpis."
           />
         </Container>
-        <div className="max-w-[1170px] mx-auto bg-[#EDEDED] h-[800px] rounded-lg shadow-md mt-12 flex justify-center items-center">
+        <div className="max-w-[1170px] mx-[24px] sm:mx-auto bg-[#EDEDED] max-h-[800px] rounded-lg shadow-md mt-12 sm:mb-[126px] flex justify-center items-center">
           <p className="text-[#888] text-lg">Image Goes Here</p>
         </div>
       </div>
       {/* Benefits Section */}
-      <div className="relative overflow-hidden">
+      <div className="relative overflow-hidden bg-[#0C0D0C]">
         <Container>
-          <div className="sm:py-[150px] py-[24px] relative z-10">
-            <div className="absolute -top-[10%] -left-[100%] pointer-events-none z-0">
-              <img src={bannerBg} alt="Background decoration" />
-            </div>
-
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-[40px] sm:gap-[130px] shrink-0">
-              <div className="flex flex-col sm:gap-2 w-full">
-                <div className="flex sm:justify-start justify-center items-center text-center gap-3">
-                  <img src={rightSideHr} alt="" className="max-w-[100px]" />
-                  <p className="text-xl font-normal leading-[30px]">Benefits</p>
-                  <img src={leftSideHr} alt="" className="max-w-[100px]" />
+          <div className="sm:py-[150px] sm:py-[24px] py-[50px] relative z-10">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-[24px] sm:gap-[130px] shrink-0">
+              <div className="flex flex-col gap-4 sm:gap-2 w-full">
+                <div className="flex justify-center sm:justify-start items-center text-center gap-3">
+                  <img
+                    src={rightSideHr}
+                    alt=""
+                    className="max-w-[80px] sm:max-w-[100px]"
+                  />
+                  <p className="text-[14px] sm:text-xl font-normal leading-[21px] sm:leading-[30px]">
+                    Benefits
+                  </p>
+                  <img
+                    src={leftSideHr}
+                    alt=""
+                    className="max-w-[80px] sm:max-w-[100px]"
+                  />
                 </div>
-                <h1 className="sm:text-5xl sm:text-4xl text-3xl font-semibold text-gradient sm:leading-[72px] leading-[60px]">
+                <h1 className="text-[24px] sm:text-4xl lg:text-5xl font-semibold text-gradient leading-[36px] sm:leading-[60px] lg:leading-[72px] text-center sm:text-left">
                   Exclusive customer Experience
                 </h1>
-                <p className="text-[#A6A6A6] text-lg font-light leading-[27px]">
+                <p className="text-[#A6A6A6] text-[12px] sm:text-lg font-light leading-[18px] sm:leading-[27px] text-center sm:text-left">
                   A perfect balance—customers get great value, and merchants
                   boost their sales effortlessly!
                 </p>
               </div>
-
-              <div className="flex flex-col gap-[30px]">
+              <div className="flex flex-col gap-[24px] sm:gap-[30px]">
                 {benefitsData.map((benefit) => (
                   <div
                     key={benefit.id}
-                    className="p-6 rounded-2xl bg-gradient-to-br from-white/10 via-white/5 to-white/5 backdrop-blur-[20px] flex flex-col gap-3 items-start"
+                    className="p-4 sm:p-6 rounded-2xl bg-gradient-to-br from-white/10 via-white/5 to-white/5 backdrop-blur-[20px] flex flex-col gap-3 items-start"
                   >
-                    <div className="p-3 rounded-sm bg-[linear-gradient(195deg,rgba(255,255,255,0.10)_0%,rgba(255,255,255,0.02)_50%,rgba(255,255,255,0.06)_100%)] backdrop-blur-[20px]">
+                    <div className="p-2 sm:p-3 rounded-sm bg-[linear-gradient(195deg,rgba(255,255,255,0.10)_0%,rgba(255,255,255,0.02)_50%,rgba(255,255,255,0.06)_100%)] backdrop-blur-[20px]">
                       <img src={benefit.image} alt="benefit" />
                     </div>
-                    <h3 className="sm:text-[32px] sm:text-[25px] text-[16px] font-normal text-[#f7f7f7] sm:leading-[48px]">
+                    <h3 className="text-[16px] sm:text-[25px] lg:text-[32px] font-normal text-[#f7f7f7] leading-[24px] sm:leading-[40px] lg:leading-[48px]">
                       {benefit.title}
                     </h3>
-                    <p className="text-base font-normal leading-[30px] text-[#A6A6A6]">
+                    <p className="text-[14px] sm:text-base font-normal leading-[24px] sm:leading-[30px] text-[#A6A6A6]">
                       {benefit.description}
                     </p>
                   </div>
@@ -132,71 +128,53 @@ const ForMerchantSection = () => {
         </Container>
       </div>
       {/* Protection Coverage Section */}
-      <div className="w-full sm:py-[150px] py-[24px] relative bg-gradient-to-r from-white/5 to-white/5 backdrop-blur-[37px] overflow-hidden">
+      <div className="w-full sm:py-[94px] py-[24px] relative bg-gradient-to-r from-white/5 to-white/5 backdrop-blur-[37px] overflow-hidden">
         <div className="absolute top-1/2 left-[-50%] transform -translate-y-1/2 pointer-events-none z-0">
           <img src={roundBg} alt="Background decoration" />
         </div>
         <div className="absolute top-1/2 right-[-50%] transform -translate-y-1/2 pointer-events-none z-0">
           <img src={roundBg} alt="Background decoration" />
         </div>
+
         <Container>
-          {/* <div className="max-w-full mx-auto flex flex-col gap-2">
-            <div className="flex justify-center items-center gap-3">
-              <img src={rightSideHr} alt="" className="max-w-[100px]" />
-              <p className="text-sm sm:text-xl font-normal sm:leading-[30px] whitespace-nowrap">
-                Protection Coverage
-              </p>
-              <img src={leftSideHr} alt="" className="max-w-[100px]" />
-            </div>
-            <div className="max-w-[620px] mx-auto">
-              <h1 className="sm:text-5xl text-3xl font-semibold text-gradient text-center sm:leading-[72px] leading-[36px]">
-                Sync with your existing checkout module.
-              </h1>
-            </div>
-            <div className="max-w-[722px] mx-auto">
-              <p className="text-[#A6A6A6] sm:text-lg text-sm font-light text-center sm:leading-[27px] leading-[18px]">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-                mollis ante eu sem tempus, eleifend sollicitudin felis cursus.
-                Praesent non ante vitae turpis.
-              </p>
-            </div>
-          </div> */}
           <SectionTitle
             topText="Protection Coverage"
             middleText="Sync with your existing checkout module."
             bottomText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mollis ante eu sem tempus, eleifend sollicitudin felis cursus Praesent non ante vitae turpis."
           />
+
           <div className="max-w-[874px] mx-auto grid grid-cols-1 md:grid-cols-2 gap-[30px] mt-[48px]">
             <div className="flex flex-col gap-3 items-start max-w-[422px]">
-              <div className="bg-[linear-gradient(195deg,rgba(255,255,255,0.10)_0%,rgba(255,255,255,0.02)_50%,rgba(255,255,255,0.06)_100%)] backdrop-blur-[20px] p-4 rounded-xl">
+              <div className="bg-[linear-gradient(195deg,rgba(255,255,255,0.10)_0%,rgba(255,255,255,0.02)_50%,rgba(255,255,255,0.06)_100%)] backdrop-blur-[20px] sm:p-4 p-2 rounded-xl">
                 <img
                   src={benefit1}
                   alt="Easy to setup icon"
-                  className="w-10 h-10"
+                  className="w-[40px] h-[40px] sm:w-10 sm:h-10"
                 />
               </div>
-              <h3 className="text-2xl font-semibold text-white">
+              <h3 className="text-[18px] sm:text-2xl font-semibold text-white leading-[28px] sm:leading-[32px]">
                 Easy to setup
               </h3>
-              <p className="text-[#A6A6A6] text-base leading-[27px]">
+              <p className="text-[#A6A6A6] text-[14px] sm:text-base leading-[24px] sm:leading-[27px]">
                 Check out our custom research services designed to tackle your
                 business challenges! We offer market analysis and consumer
                 insights to help you make better decisions and boost your
                 growth.
               </p>
             </div>
+
             <div className="flex flex-col gap-3 items-start max-w-[422px]">
-              <div className="bg-[linear-gradient(195deg,rgba(255,255,255,0.10)_0%,rgba(255,255,255,0.02)_50%,rgba(255,255,255,0.06)_100%)] backdrop-blur-[20px] p-4 rounded-xl">
+              <div className="bg-[linear-gradient(195deg,rgba(255,255,255,0.10)_0%,rgba(255,255,255,0.02)_50%,rgba(255,255,255,0.06)_100%)] backdrop-blur-[20px] sm:p-4 p-2 rounded-xl">
                 <img
                   src={benefit2}
                   alt="No backhand branding icon"
-                  className="w-10 h-10"
+                  className="w-[40px] h-[40px] sm:w-10 sm:h-10"
                 />
               </div>
-              <h3 className="text-2xl font-semibold text-white">
+              <h3 className="text-[18px] sm:text-2xl font-semibold text-white leading-[28px] sm:leading-[32px]">
                 No Backhand Branding
               </h3>
-              <p className="text-[#A6A6A6] text-base leading-[27px]">
+              <p className="text-[#A6A6A6] text-[14px] sm:text-base leading-[24px] sm:leading-[27px]">
                 Check out our custom research services designed to tackle your
                 business challenges! We offer market analysis and consumer
                 insights to help you make better decisions and boost your
@@ -208,71 +186,81 @@ const ForMerchantSection = () => {
       </div>
 
       {/* Support Section */}
-      <Container>
-        <div className="sm:py-[150px] py-[50px]">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-[30px]">
-            <div className="flex flex-col gap-4 md:max-w-[570px] w-full">
-              <div className="flex sm:justify-start justify-center items-center gap-3">
-                <img src={rightSideHr} alt="" className="max-w-[100px]" />
-                <p className="text-xl font-normal leading-[30px]">Support</p>
-                <img src={leftSideHr} alt="" className="max-w-[100px]" />
+      <div className="bg-[#0C0D0C]">
+        <Container>
+          <div className="sm:py-[150px] py-[50px]">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-[30px]">
+              <div className="flex flex-col gap-4 md:max-w-[570px] w-full">
+                <div className="flex sm:justify-start justify-center items-center gap-3">
+                  <img src={rightSideHr} alt="" className="max-w-[100px]" />
+                  <p className="text-[14px] sm:text-xl font-normal leading-[21px] sm:leading-[30px]">
+                    Support
+                  </p>
+                  <img src={leftSideHr} alt="" className="max-w-[100px]" />
+                </div>
+
+                <h2 className="text-[24px] sm:text-5xl font-semibold text-gradient leading-[36px] sm:leading-[72px] text-center sm:text-left">
+                  Instant support and service
+                </h2>
+
+                <p className="text-[#A6A6A6] text-[12px] sm:text-base font-light leading-[18px] sm:leading-relaxed text-center sm:text-left">
+                  A perfect balance—customers get great value, and merchants
+                  boost their sales effortlessly!
+                </p>
+
+                <div className="sm:mt-10 mt-[32px] flex flex-col gap-6 w-full">
+                  <div className="flex items-start sm:items-center gap-4">
+                    <div className="bg-[#1A1A1A] p-3 rounded-md w-[55px] h-[56px] flex items-center justify-center">
+                      <img
+                        src={writingIcon}
+                        alt="Seamless claim icon"
+                        className="w-[40px] h-[40px]"
+                      />
+                    </div>
+                    <p className="text-white text-[14px] sm:text-base leading-[22px] sm:leading-[26px]">
+                      Seamless claim submission experience
+                    </p>
+                  </div>
+
+                  <div className="flex items-start sm:items-center gap-4">
+                    <div className="bg-[#1A1A1A] p-3 rounded-md w-[55px] h-[56px] flex items-center justify-center">
+                      <img
+                        src={clockIcon}
+                        alt="Fast claim processing icon"
+                        className="w-[40px] h-[40px]"
+                      />
+                    </div>
+                    <p className="text-white text-[14px] sm:text-base leading-[22px] sm:leading-[26px]">
+                      Fast and transparent claim processing
+                    </p>
+                  </div>
+
+                  <div className="flex items-start sm:items-center gap-4">
+                    <div className="bg-[#1A1A1A] p-3 rounded-md w-[55px] h-[56px] flex items-center justify-center">
+                      <img
+                        src={supportIcon}
+                        alt="Expert support icon"
+                        className="w-[40px] h-[40px]"
+                      />
+                    </div>
+                    <p className="text-white text-[14px] sm:text-base leading-[22px] sm:leading-[26px]">
+                      Dedicated expert support from start to finish
+                    </p>
+                  </div>
+                </div>
               </div>
-              <h2 className="sm:text-5xl text-3xl font-semibold text-gradient leading-[72px] text-center md:text-left">
-                Instant support and service
-              </h2>
-              <p className="text-[#A6A6A6] text-base leading-relaxed text-center md:text-left">
-                A perfect balance—customers get great value, and merchants boost
-                their sales effortlessly!
-              </p>
-              <div className="mt-10 flex flex-col gap-6 w-full">
-                <div className="flex items-center gap-4">
-                  <div className="bg-[#1A1A1A] p-3 rounded-md w-[55px] h-[56px] flex items-center justify-center">
-                    <img
-                      src={writingIcon}
-                      alt="Seamless claim icon"
-                      className="w-full"
-                    />
-                  </div>
-                  <p className="text-white text-base">
-                    Seamless claim submission experience
-                  </p>
-                </div>
-                <div className="flex items-center gap-4">
-                  <div className="bg-[#1A1A1A] p-3 rounded-md w-[55px] h-[56px] flex items-center justify-center">
-                    <img
-                      src={clockIcon}
-                      alt="Fast claim processing icon"
-                      className="w-full"
-                    />
-                  </div>
-                  <p className="text-white text-base">
-                    Fast and transparent claim processing
-                  </p>
-                </div>
-                <div className="flex items-center gap-4">
-                  <div className="bg-[#1A1A1A] p-3 rounded-md w-[55px] h-[56px] flex items-center justify-center">
-                    <img
-                      src={supportIcon}
-                      alt="Expert support icon"
-                      className="w-full"
-                    />
-                  </div>
-                  <p className="text-white text-base">
-                    Dedicated expert support from start to finish
-                  </p>
-                </div>
+
+              <div className="flex justify-center md:justify-end w-full mt-10 md:mt-0">
+                <img
+                  src={supportImage}
+                  alt="Support team"
+                  className="rounded-xl w-full max-w-[570px]"
+                />
               </div>
-            </div>
-            <div className="flex justify-center md:justify-end w-full">
-              <img
-                src={supportImage}
-                alt="Support team"
-                className="rounded-xl w-full max-w-[570px]"
-              />
             </div>
           </div>
-        </div>
-      </Container>
+        </Container>
+      </div>
     </>
   );
 };
