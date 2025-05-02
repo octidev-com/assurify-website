fb9c62947072a4378e3ff49bfb2dbb1ac3a7e516
Mahmudul Haque Noor <mahmudulhaquenoor@gmail.com>
2025-05-02 13:02:57 +0600

    fix: navbarSpacer height fix for announcement visible

--- src/component/Common/NavbarSpacer.jsx
+++ src/component/Common/NavbarSpacer.jsx
@@ -1,6 +1,6 @@
 const NavbarSpacer = ({ isAnnouncementVisible }) => {
   const spacerHeight = isAnnouncementVisible
-    ? 'h-[125px]'
+    ? 'h-[100px]'
     : 'h-[48px] sm:h-[56px] lg:h-[97px]'
 
   return <div className={`w-full ${spacerHeight}`}></div>
