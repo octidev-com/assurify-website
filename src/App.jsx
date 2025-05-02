ee585f5111c0b3dc40372fae1c9c4814e962c1e5
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2025-05-02 08:54:05 +0600

    fix: add scroll restoration

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,46 @@
-import React, { useState } from 'react'
-import Navbar from './component/Navbar'
-import Footer from './component/Footer'
-import Home from './pages/Home'
-import { Route, Routes } from 'react-router'
-import Pricing from './pages/Pricing'
-import ContactUs from './pages/ContactUs'
-import Deals from './pages/Deals'
-import Checkout from './pages/Checkout'
-import ForMerchant from './pages/ForMerchant'
-import PrivacyPolicy from './pages/PrivacyPolicy'
-import NavbarSpacer from './component/Common/NavbarSpacer'
+import React, { useState } from "react";
+import Navbar from "./component/Navbar";
+import Footer from "./component/Footer";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router";
+import Pricing from "./pages/Pricing";
+import ContactUs from "./pages/ContactUs";
+import Deals from "./pages/Deals";
+import Checkout from "./pages/Checkout";
+import ForMerchant from "./pages/ForMerchant";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import NavbarSpacer from "./component/Common/NavbarSpacer";
+import ScrollRestoration from "./component/Common/ScrollRestoration";
 
 const App = () => {
-  const [isAnnouncementVisible, setIsAnnouncementVisible] = useState(true)
+  const [isAnnouncementVisible, setIsAnnouncementVisible] = useState(true);
 
   return (
-    <div className='w-full mx-auto bg-[#0C0D0C] text-[#fff] min-h-screen'>
+    <div className="w-full mx-auto bg-[#0C0D0C] text-[#fff] min-h-screen">
       {/* Fixed Navbar */}
-      <div className='fixed top-0 left-0 w-full z-[100]'>
-        <Navbar
-          isAnnouncementVisible={isAnnouncementVisible}
-          setIsAnnouncementVisible={setIsAnnouncementVisible}
-        />
+      <div className="fixed top-0 left-0 w-full z-[100]">
+        <Navbar isAnnouncementVisible={isAnnouncementVisible} setIsAnnouncementVisible={setIsAnnouncementVisible} />
       </div>
+
+      {/* Scroll Restoration */}
+      <ScrollRestoration />
 
       {/* Spacer with dynamic height */}
       <NavbarSpacer isAnnouncementVisible={isAnnouncementVisible} />
 
       {/* Content */}
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/pricing' element={<Pricing />} />
-        <Route path='/checkout' element={<Checkout />} />
-        <Route path='/forMerchant' element={<ForMerchant />} />
-        <Route path='/contact-us' element={<ContactUs />} />
-        <Route path='/deals' element={<Deals />} />
-        <Route path='/privacy-policy' element={<PrivacyPolicy />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/forMerchant" element={<ForMerchant />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/deals" element={<Deals />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       </Routes>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
