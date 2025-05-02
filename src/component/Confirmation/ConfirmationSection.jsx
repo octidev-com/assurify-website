5b6da8c60c0c0729a97e1a7df227ffd6cf418d68
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2025-05-02 11:52:56 +0600

    refactor: remove padding

--- src/component/Confirmation/ConfirmationSection.jsx
+++ src/component/Confirmation/ConfirmationSection.jsx
@@ -4,14 +4,10 @@ import SectionTitle from "../Common/SectionTitle";
 
 const ConfirmationSection = () => {
   return (
-    <div className="max-w-[646px] mx-auto lg:px-6 pt-8 pb-12 mt-[56px]">
+    <div className="max-w-[646px] mx-auto mt-[56px]">
       {/* Image Section */}
       <div className="flex justify-center mb-6">
-        <img
-          src={ThankYouImg}
-          alt="Thank you illustration"
-          className="w-[278px] h-[300px]"
-        />
+        <img src={ThankYouImg} alt="Thank you illustration" className="w-[278px] h-[300px]" />
       </div>
 
       {/* Headings */}
@@ -20,10 +16,8 @@ const ConfirmationSection = () => {
         bottomText="Your purchase has been successfully completed. We are thrilled to have you on board!"
       />
       <p className="mx-auto text-[#A6A6A6] text-[12px] lg:text-lg font-light text-center leading-[18px] lg:leading-[27px] mt-6 mb-6">
-        With Assurify Shipping Protection, your orders are now backed by fast,
-        reliable, and hassle-free coverage.
-        <br /> No more worrying about lost, stolen, or damaged packages — we've
-        got you covered!
+        With Assurify Shipping Protection, your orders are now backed by fast, reliable, and hassle-free coverage.
+        <br /> No more worrying about lost, stolen, or damaged packages — we've got you covered!
       </p>
 
       {/* Order summary section */}
@@ -39,9 +33,7 @@ const ConfirmationSection = () => {
           <div className="flex justify-between text-gray-400 text-sm">
             <div>
               <p>Promo Code</p>
-              <p className="text-xs mt-1">
-                You have 180 days for using this promo code
-              </p>
+              <p className="text-xs mt-1">You have 180 days for using this promo code</p>
             </div>
             <p className="text-right">Assurify1401</p>
           </div>
@@ -58,11 +50,9 @@ const ConfirmationSection = () => {
         <div>
           <p className="leading-[27px">Here's what happens next:</p>
           <p className="leading-[27px]">
-            • You'll receive a confirmation email with all your protection
-            details.
-            <br />• If you ever experience any shipping issues, simply file a
-            claim through our easy online process — and we'll take care of the
-            rest.
+            • You'll receive a confirmation email with all your protection details.
+            <br />• If you ever experience any shipping issues, simply file a claim through our easy online process — and we'll take care of
+            the rest.
           </p>
         </div>
 
@@ -72,10 +62,7 @@ const ConfirmationSection = () => {
           <p className="leading-[27px]">
             Our support team is always ready to assist you.
             <br /> Reach out to us anytime at{" "}
-            <a
-              href="mailto:info@assurityfusion.com"
-              className="text-[#5FD4FF] underline"
-            >
+            <a href="mailto:info@assurityfusion.com" className="text-[#5FD4FF] underline">
               info@assurityfusion.com
             </a>
             .
@@ -92,7 +79,7 @@ const ConfirmationSection = () => {
 
       {/* Back to Home Button */}
       <div className="flex justify-center mt-6">
-        <Button buttonText="Back To Home" variant="primary-green" />
+        <Button variant="primary-green">Back To Home</Button>
       </div>
     </div>
   );
