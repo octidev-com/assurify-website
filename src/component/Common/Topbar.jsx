1cee49ae54edb381d79ebba347cc7691b970308b
Mahmudul Haque Noor <mahmudulhaquenoor@gmail.com>
2025-05-02 09:40:34 +0600

    refactor: change top bar spacing and text size

--- src/component/Common/Topbar.jsx
+++ src/component/Common/Topbar.jsx
@@ -6,10 +6,10 @@ const Topbar = ({ isAnnouncementVisible, setIsAnnouncementVisible }) => {
     <div className='bg-[rgba(4,3,3,0.12)] backdrop-blur-[37px]'>
       <Container>
         <div
-          className={`justify-between items-center py-4 ${
+          className={`justify-between items-center py-[4px] ${
             isAnnouncementVisible ? 'flex' : 'hidden'
           }`}>
-          <p className='text-[#f7f7f7] text-sm md:text-base font-normal leading-[21px] md:leading-[24px]'>
+          <p className='text-[#f7f7f7] text-sm  font-normal leading-[21px] '>
             Enjoy 20% OFF your next purchase with code SAVE20 - limited time
             only! <span className='underline'>Click Here</span>
           </p>
