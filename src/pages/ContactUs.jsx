895f03d8fd997d7adadc487e1f1edcf2ace6a203
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2025-05-06 15:29:48 +0600

    refactor: add contact us form component

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -3,29 +3,9 @@ import Container from "../component/Common/Container";
 import SectionTitle from "../component/Common/SectionTitle";
 import Button from "../component/Common/Button";
 import TopBg from "../component/Common/TopBg";
-import { useFormik } from "formik";
-import { contactUsSchema } from "../schemas/forms";
-
-// Initial values for the form fields
-const initialValues = {
-  fullName: "",
-  emailAddress: "",
-  subject: "",
-  message: "",
-};
-
-// Function to handle form submission
-const onSubmit = (values) => {
-  console.log("Form submitted", values);
-};
+import ContactForm from "../component/Forms/ContactForm";
 
 const ContactUs = () => {
-  const { values, errors, touched, handleBlur, handleChange, handleSubmit } = useFormik({
-    initialValues: initialValues,
-    validationSchema: contactUsSchema,
-    onSubmit: onSubmit,
-  });
-
   return (
     <TopBg>
       {/* Main content */}
@@ -79,89 +59,8 @@ const ContactUs = () => {
 
             {/* Contact Us Form */}
             <div className="p-4 sm:p-5 lg:p-6 rounded-[16px] sm:rounded-[20px] lg:rounded-[26px] bg-gradient-to-r from-[rgba(255,255,255,0.03)] to-[rgba(255,255,255,0.03)] backdrop-blur-[37px] w-full lg:w-[60%]">
-              <form onSubmit={handleSubmit} className="flex flex-col gap-4 sm:gap-5 lg:gap-6">
-                <div>
-                  <label htmlFor="fullName" className="text-sm sm:text-base font-light leading-5 sm:leading-6 text-[#A6A6A6]">
-                    Full Name
-                  </label>
-                  <input
-                    type="text"
-                    id="fullName"
-                    name="fullName"
-                    value={values.fullName}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    className={`${
-                      errors.fullName && touched.fullName ? "border-red-500" : "border-[#A6A6A6]"
-                    } w-full px-3 sm:px-4 h-10 sm:h-11 lg:h-12 rounded-xl sm:rounded-2xl mt-1 sm:mt-2 border border-[#A6A6A6] opacity-[0.6] focus:outline-none text-sm sm:text-base`}
-                  />
-
-                  {/* Error message */}
-                  {errors.fullName && touched.fullName && <p className="text-red-500 text-sm mt-1">{errors.fullName}</p>}
-                </div>
-                <div>
-                  <label htmlFor="email-address" className="text-sm sm:text-base font-light leading-5 sm:leading-6 text-[#A6A6A6]">
-                    Email Address
-                  </label>
-                  <input
-                    type="email"
-                    id="emailAddress"
-                    name="emailAddress"
-                    value={values.emailAddress}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    className={`${
-                      errors.emailAddress && touched.emailAddress ? "border-red-500" : "border-[#A6A6A6]"
-                    } w-full px-3 sm:px-4 h-10 sm:h-11 lg:h-12 rounded-xl sm:rounded-2xl mt-1 sm:mt-2 border border-[#A6A6A6] opacity-[0.6] focus:outline-none text-sm sm:text-base`}
-                  />
-
-                  {/* Error message */}
-                  {errors.emailAddress && touched.emailAddress && <p className="text-red-500 text-sm mt-1">{errors.emailAddress}</p>}
-                </div>
-                <div>
-                  <label htmlFor="subject" className="text-sm sm:text-base font-light leading-5 sm:leading-6 text-[#A6A6A6]">
-                    Subject
-                  </label>
-                  <input
-                    type="text"
-                    id="subject"
-                    name="subject"
-                    value={values.subject}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    className={`${
-                      errors.subject && touched.subject ? "border-red-500" : "border-[#A6A6A6]"
-                    } w-full px-3 sm:px-4 h-10 sm:h-11 lg:h-12 rounded-xl sm:rounded-2xl mt-1 sm:mt-2 border border-[#A6A6A6] opacity-[0.6] focus:outline-none text-sm sm:text-base`}
-                  />
-
-                  {/* Error message */}
-                  {errors.subject && touched.subject && <p className="text-red-500 text-sm mt-1">{errors.subject}</p>}
-                </div>
-                <div>
-                  <label
-                    htmlFor="what-can-we-help-you-with"
-                    className="text-sm sm:text-base font-light leading-5 sm:leading-6 text-[#A6A6A6]"
-                  >
-                    What can we help you with?
-                  </label>
-                  <textarea
-                    id="message"
-                    name="message"
-                    value={values.message}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    className={`${
-                      errors.message && touched.message ? "border-red-500" : "border-[#A6A6A6]"
-                    } w-full p-3 sm:p-4 h-[120px] sm:h-[160px] lg:h-[200px] rounded-lg sm:rounded-xl mt-1 sm:mt-2 border border-[#A6A6A6] opacity-[0.6] focus:outline-none text-sm sm:text-base`}
-                  />
-
-                  {/* Error message */}
-                  {errors.message && touched.message && <p className="text-red-500 text-sm mt-1">{errors.message}</p>}
-                </div>
-                <button type="submit" className="btn-primary-green-full">
-                  Send Message
-                </button>
-              </form>
+              {/* CONTACT US FORM */}
+              <ContactForm />
             </div>
           </div>
         </div>
