02fa49a1dfd0a1bc0d3f94c1ff5edea183d8fed4
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2025-05-13 19:03:40 +0600

    feat: show loading spinner

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,39 +1,54 @@
+// With loading state which does not work
 import { CheckoutProvider } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import { useState } from "react";
 import Container from "../component/Common/Container";
 import SectionTitle from "../component/Common/SectionTitle";
 import CheckoutForm from "../component/Forms/CheckoutForm";
-import { useLocation } from "react-router";
+import LoadingSpinner from "../component/Common/LoadingSpinner";
 
 const Checkout = () => {
-  const location = useLocation();
-  const price = location.state.price;
-  const plan = location.state.plan;
+  const [isLoading, setIsLoading] = useState(true);
 
-  // load stripe using stripe publishable key
+  // Load stripe using stripe publishable key
   const stripe = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 
   const fetchClientSecret = async () => {
-    const response = await fetch("http://localhost:3000/create-checkout-session", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ plan: plan, price: price }),
-    });
-    const json = await response.json();
-    return json.checkoutSessionClientSecret;
+    try {
+      const response = await fetch("http://localhost:3000/create-checkout-session", {
+        method: "POST",
+      });
+      const json = await response.json();
+      console.log(json.checkoutSessionClientSecret);
+      const secret = json.checkoutSessionClientSecret;
+
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 500); // Simulate a delay of 1 second
+
+      return secret;
+    } catch (error) {
+      console.error("Error fetching client secret:", error);
+      return null;
+    }
   };
+
+  // console.log(isLoading);
   return (
-    <div>
+    <div className="pt-[32px] md:pt-[56px]">
       <Container>
         {/* MAIN SECTION */}
         <SectionTitle middleText={"Checkout"} />
 
-        <div className="mt-[32px] lg:mt-14 mx-auto px-4 lg:px-0 min-h-screen">
+        <div className="mt-[24px] lg:mt-[32px] mx-auto min-h-[90vh]">
+          {/* {isLoading ? (
+            <div className="text-center">Loading...</div>
+          ) : ( */}
+          {isLoading && <LoadingSpinner />}
           <CheckoutProvider stripe={stripe} options={{ fetchClientSecret }}>
-            <CheckoutForm />
+            {!isLoading && <CheckoutForm />}
           </CheckoutProvider>
+          {/* )} */}
         </div>
       </Container>
     </div>
