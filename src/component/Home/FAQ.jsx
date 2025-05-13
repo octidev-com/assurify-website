b06828ba2f392c4c5c8f6db5eed454d16264c751
Mahmudul Haque Noor <mahmudulhaquenoor@gmail.com>
2025-05-13 16:30:45 +0600

    refactor: pricing page faq content change with real content

--- src/component/Home/FAQ.jsx
+++ src/component/Home/FAQ.jsx
@@ -1,32 +1,31 @@
-import React, { useState } from "react";
-import CustomIcon from "../../assets/icon/CustomIcon";
-import SectionTitle from "../Common/SectionTitle";
-import Container from "../Common/Container";
+import React, { useState } from 'react'
+import CustomIcon from '../../assets/icon/CustomIcon'
+import SectionTitle from '../Common/SectionTitle'
+import Container from '../Common/Container'
 
 const FAQ = ({ faqs }) => {
-  const [openFAQ, setOpenFAQ] = useState(null);
+  const [openFAQ, setOpenFAQ] = useState(null)
 
   const toggleFAQ = (id) => {
     setOpenFAQ(openFAQ === id ? null : id)
   }
 
   return (
-    <div className="mt-[50px] lg:mt-[150px]">
+    <div className='mt-[50px] lg:mt-[150px]'>
       {/* FAQ Section */}
       <Container>
-      <SectionTitle
-          topText={"Customer Support"}
-          middleText={"Frequently Asked Question"}
+        <SectionTitle
+          topText={'Customer Support'}
+          middleText={'Frequently Asked Question'}
           bottomText={
-            "Find answers to common questions and get the support you need—quick and easy"
+            'Find answers to common questions and get the support you need—quick and easy'
           }
         />
         <div className='max-w-[770px] mx-auto mt-8 lg:mt-12'>
           {faqs.map((faq) => (
             <div
               key={faq.id}
-              className="bg-gradient-to-r from-white/5 to-white/5 backdrop-blur-[37px] rounded-2xl mb-3 lg:mb-6 overflow-hidden"
-            >
+              className='bg-gradient-to-r from-white/5 to-white/5 backdrop-blur-[37px] rounded-2xl mb-3 lg:mb-6 overflow-hidden'>
               <button
                 className='flex items-center gap-4 p-4 w-full text-left'
                 onClick={() => toggleFAQ(faq.id)}>
@@ -45,12 +44,12 @@ const FAQ = ({ faqs }) => {
               <div
                 className={`transition-all duration-300 ease-in-out ${
                   openFAQ === faq.id
-                    ? 'max-h-40 opacity-100'
+                    ? 'max-h-56 opacity-100'
                     : 'max-h-0 opacity-0'
                 } overflow-hidden`}>
-                <p className='p-4 pt-0 text-[#A6A6A6] text-[12px] lg:text-base font-light leading-[18px] lg:leading-6'>
+                <div className='p-4 pt-0 text-[#A6A6A6] text-[12px] lg:text-base font-light leading-[18px] lg:leading-6'>
                   {faq.answer}
-                </p>
+                </div>
               </div>
             </div>
           ))}
--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,67 +1,129 @@
-import Container from "../component/Common/Container";
-import PricingSection from "../component/Home/PricingSection";
-import TopBg from "../component/Common/TopBg";
-import FAQ from "../component/Home/FAQ";
-import { Helmet } from "react-helmet";
+import PricingSection from '../component/Home/PricingSection'
+import TopBg from '../component/Common/TopBg'
+import FAQ from '../component/Home/FAQ'
+import { Helmet } from 'react-helmet'
 
 const Pricing = () => {
   const faqs = [
     {
       id: 1,
-      question: "Is there a free trial available",
+      question: 'Is there a free trial available for all plans?',
       answer:
-        "Yes! We offer a free trial so you can explore all features risk-free. No credit card required experience the benefits  and see how we can help your business grow effortlessly!",
+        'Yes! Every package, including the Lifetime Deal, comes with a 15-day free trial to try the Assurify Shipping Protection App with full confidence.'
     },
     {
       id: 2,
-      question: "What is shipping protection, and how does it work?",
-      answer:
-        "Yes! We offer a free trial so you can explore all features risk-free. No credit card required experience the benefits  and see how we can help your business grow effortlessly!",
+      question: "What's included in the Free Plan?",
+      answer: (
+        <div>
+          The Free Plan is entirely free and consists of:
+          <ul className='list-disc pl-5'>
+            <li>50 safe and secure orders/month</li>
+            <li>Claim Management</li>
+            <li>Basic Analytics</li>
+            <li>Chat Support</li>
+            <li>It's ideal for small businesses or those just beginning</li>
+          </ul>
+        </div>
+      )
     },
     {
       id: 3,
-      question: "How do I add shipping protection to my order?",
-      answer:
-        "Yes! We offer a free trial so you can explore all features risk-free. No credit card required experience the benefits  and see how we can help your business grow effortlessly!",
+      question: "What's the difference between Starter and Starter Plus?",
+      answer: (
+        <div>
+          <span className='font-semibold'>Starter: </span>
+          600 safe orders/month, Return Management, Basic Analytics
+          <br />
+          <span className='font-semibold'>Beginner Plus: </span>
+          Beginner Plus includes 2500 secure orders/month, Checkout Extension,
+          Advanced Analytics, and all Starter functions. Beginner Plus is much
+          better suited for growing shops needing deeper insights and more
+          automation.
+        </div>
+      )
     },
     {
       id: 4,
-      question: "How do I file a claim a damaged or missing order?",
+      question: 'For a 15-day trial, do I need to enter payment details?',
       answer:
-        "Yes! We offer a free trial so you can explore all features risk-free. No credit card required experience the benefits  and see how we can help your business grow effortlessly!",
+        "No, you can start the trial without entering your card information. Attempt all functions first-- dedicate just when you're prepared."
     },
     {
       id: 5,
-      question: "Is there a time limit for filing a claim?",
-      answer:
-        "Yes! We offer a free trial so you can explore all features risk-free. No credit card required experience the benefits  and see how we can help your business grow effortlessly!",
+      question: 'What is the Checkout Extension, and which plans include it?',
+      answer: (
+        <div>
+          The Checkout Extension lets customers add shipping defense during
+          checkout. It's included in:
+          <ul className='list-disc pl-5'>
+            <li>Starter</li>
+            <li>Starter Plus</li>
+            <li>Business</li>
+            <li>Ultimate</li>
+            <li>Lifetime Deal</li>
+          </ul>
+        </div>
+      )
     },
     {
       id: 6,
-      question: "How long does it take to process a claim?",
+      question: 'What type of assistance is included in each plan?',
       answer:
-        "Yes! We offer a free trial so you can explore all features risk-free. No credit card required experience the benefits  and see how we can help your business grow effortlessly!",
+        'All strategies come with Chat Support and On-Call Assistance. However, the Ultimate and Lifetime Deal plans also offer Priority Support for faster resolutions.'
     },
     {
       id: 7,
-      question: "Who do I contact for help with a claim?",
+      question: 'What does "safe and secure orders/month" suggest?',
       answer:
-        "Yes! We offer a free trial so you can explore all features risk-free. No credit card required experience the benefits  and see how we can help your business grow effortlessly!",
+        "This describes the variety of orders safeguarded under the Assurify Shipping Protection system. Each strategy has a limit, unless you're on Enterprise or Ultimate, which use unlimited safeguarded orders."
     },
-  ];
+    {
+      id: 8,
+      question:
+        'Is there an advantage to choosing the Annual Plan vs. the Monthly Plan?',
+      answer:
+        'Yes. The annual plans are priced at a significant discount compared to month-to-month billing. You also get undisturbed service and comfort.'
+    },
+    {
+      id: 9,
+      question: "What's included in the Lifetime Deal?",
+      answer: (
+        <div>
+          The Lifetime Deal is a one-time payment of $1140 and consists of:
+          <ul className='list-disc pl-5'>
+            <li>Lifetime Access</li>
+            <li>Unlimited protected orders</li>
+            <li>Checkout Extension</li>
+            <li>Claim & Return Management</li>
+            <li>Advanced Analytics</li>
+            <li>Chat & Priority Support</li>
+            <li>Seasonal Updates</li>
+          </ul>
+          It's perfect for businesses searching for long-lasting cost savings.
+        </div>
+      )
+    },
+    {
+      id: 10,
+      question: 'Can I update my strategy as my service grows?',
+      answer:
+        "Definitely! You can upgrade anytime to match your shop's requirements, whether moving from Starter to Starter Plus or opting for the Lifetime Deal."
+    }
+  ]
   return (
     <>
-    <Helmet>
-    <title>Assurify | Pricing</title>
-    </Helmet>
-    <TopBg>
-      <div className="pt-[32px] md:pt-[56px]">
-        <PricingSection />
-        <FAQ faqs={faqs} />
-      </div>
-    </TopBg>
+      <Helmet>
+        <title>Assurify | Pricing</title>
+      </Helmet>
+      <TopBg>
+        <div className='pt-[32px] md:pt-[56px]'>
+          <PricingSection />
+          <FAQ faqs={faqs} />
+        </div>
+      </TopBg>
     </>
-  );
-};
+  )
+}
 
 export default Pricing
