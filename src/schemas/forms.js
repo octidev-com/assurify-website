35b786a21524ada20b9c029e15306034f8eb9fc1
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2025-05-13 19:03:06 +0600

    refactor: remove unused form schemas

--- src/schemas/forms.js
+++ src/schemas/forms.js
@@ -1,16 +1,8 @@
 import * as yup from "yup";
 
 export const checkoutSchema = yup.object().shape({
-  firstName: yup.string().required("First name is required"),
-  lastName: yup.string().required("Last name is required"),
-  city: yup.string().required("City is required"),
-  address: yup.string().required("Address is required"),
   emailAddress: yup.string().email("Please enter a valid email address").required("Email address is required"),
-  phoneNumber: yup
-    .string()
-    .required("Phone number is required")
-    .matches(/^\+?[0-9]\d{1,14}$/, "Please enter a valid phone number"),
-  orderNote: yup.string(), // Optional field
+  storeAddress: yup.string().required("Store address is required"),
 });
 
 export const contactUsSchema = yup.object().shape({
