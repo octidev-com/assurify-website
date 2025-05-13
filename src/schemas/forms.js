458e9830727d8690b7f444a744d648dcad82a0fe
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2025-05-13 13:23:04 +0600

    refactor: remove country schema

--- src/schemas/forms.js
+++ src/schemas/forms.js
@@ -3,7 +3,6 @@ import * as yup from "yup";
 export const checkoutSchema = yup.object().shape({
   firstName: yup.string().required("First name is required"),
   lastName: yup.string().required("Last name is required"),
-  // country: yup.string().required("Country is required"),
   city: yup.string().required("City is required"),
   address: yup.string().required("Address is required"),
   emailAddress: yup.string().email("Please enter a valid email address").required("Email address is required"),
