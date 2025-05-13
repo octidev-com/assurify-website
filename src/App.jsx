4ae2f2eccbfa993856975e510fc1d8c943711294
Mahmudul Haque Noor <mahmudulhaquenoor@gmail.com>
2025-05-13 17:13:02 +0600

    refactor: wrap whole app with our topbg component

--- src/App.jsx
+++ src/App.jsx
@@ -13,42 +13,45 @@ import Confirmation from './pages/Confirmation'
 import NavbarSpacer from './component/Common/NavbarSpacer'
 import ScrollRestoration from './component/Common/ScrollRestoration'
 import NotFound from './pages/NotFound'
+import TopBg from './component/Common/TopBg'
 
 const App = () => {
   const [isAnnouncementVisible, setIsAnnouncementVisible] = useState(true)
 
   return (
-    <div className='w-full mx-auto bg-[#0C0D0C] text-[#fff] min-h-screen'>
-      {/* Fixed Navbar */}
-      <div className='fixed top-0 left-0 w-full z-[100]'>
-        <Navbar
-          isAnnouncementVisible={isAnnouncementVisible}
-          setIsAnnouncementVisible={setIsAnnouncementVisible}
-        />
+    <TopBg>
+      <div className='w-full mx-auto bg-[#0C0D0C] text-[#fff] min-h-screen'>
+        {/* Fixed Navbar */}
+        <div className='fixed top-0 left-0 w-full z-[100]'>
+          <Navbar
+            isAnnouncementVisible={isAnnouncementVisible}
+            setIsAnnouncementVisible={setIsAnnouncementVisible}
+          />
+        </div>
+
+        {/* Scroll Restoration */}
+        <ScrollRestoration />
+
+        {/* Spacer with dynamic height */}
+        <NavbarSpacer isAnnouncementVisible={isAnnouncementVisible} />
+
+        {/* Content */}
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/pricing' element={<Pricing />} />
+          <Route path='/checkout' element={<Checkout />} />
+          <Route path='/forMerchant' element={<ForMerchant />} />
+          <Route path='/confirmation' element={<Confirmation />} />
+          <Route path='/contact-us' element={<ContactUs />} />
+          <Route path='/deals' element={<Deals />} />
+          <Route path='/privacy-policy' element={<PrivacyPolicy />} />
+
+          {/* Catch all route for undefined paths */}
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        <Footer />
       </div>
-
-      {/* Scroll Restoration */}
-      <ScrollRestoration />
-
-      {/* Spacer with dynamic height */}
-      <NavbarSpacer isAnnouncementVisible={isAnnouncementVisible} />
-
-      {/* Content */}
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/pricing' element={<Pricing />} />
-        <Route path='/checkout' element={<Checkout />} />
-        <Route path='/forMerchant' element={<ForMerchant />} />
-        <Route path='/confirmation' element={<Confirmation />} />
-        <Route path='/contact-us' element={<ContactUs />} />
-        <Route path='/deals' element={<Deals />} />
-        <Route path='/privacy-policy' element={<PrivacyPolicy />} />
-
-        {/* Catch all route for undefined paths */}
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-      <Footer />
-    </div>
+    </TopBg>
   )
 }
 
