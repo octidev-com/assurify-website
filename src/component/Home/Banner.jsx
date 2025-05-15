2ffc94aadcf7209856271be8e124a9dcb4e00ea2
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2025-05-15 17:54:19 +0600

    feat: add style guide class

--- src/component/Home/Banner.jsx
+++ src/component/Home/Banner.jsx
@@ -1,39 +1,28 @@
-import React from 'react'
-import Container from '../Common/Container'
-import Button from '../Common/Button'
+import React from "react";
+import Container from "../Common/Container";
+import Button from "../Common/Button";
 const Banner = () => {
   return (
     <div>
-      <div className='max-w-[1170px] mx-auto px-[38px] xl:px-0'>
-        <div className='flex flex-col items-center gap-[12px] lg:gap-[16px] text-left md:text-center'>
-          <p className='text-[#F7F7F7] text-[48px] lg:text-[124px] font-semibold leading-[62px] lg:leading-[130px] max-w-[888px]'>
-            Eliminate Shipping Risk
+      <div className="max-w-[1170px] mx-auto px-[38px] xl:px-0">
+        <div className="flex flex-col items-center gap-[12px] lg:gap-[16px] text-left md:text-center">
+          <p className="text-display-semibold text-center max-w-[888px]">Eleminate Shipping Risk</p>
+          <p className="text-large-paragraph text-large-paragraph-light text-gray text-center max-w-[770px] inline-block">
+            Protect your packages from loss, theft, or damage and boost revenue with the{" "}
+            <h1 className="inline text-inherit m-0 p-0">Assurify Shipping Protection</h1> App, your store's shield for customer trust and
+            retention.
           </p>
-          <p className='text-center text-xs sm:text-base lg:text-xl font-light text-[#F7F7F7] leading-[18px] lg:leading-[30px] max-w-[570px] inline-block'>
-            Protect your packages from loss, theft, or damage and boost revenue
-            with the{' '}
-            <h1 className='inline text-inherit m-0 p-0'>
-              Assurify Shipping Protection
-            </h1>{' '}
-            App, your store's shield for customer trust and retention.
-          </p>
-          <div className='flex justify-center gap-4 sm:gap-5 lg:gap-7 mt-[4px] lg:mt-[16px]'>
-            <a
-              href='https://app.assurify.app/'
-              target='_blank'
-              rel='noopener noreferrer'>
+          <div className="flex justify-center gap-4 sm:gap-5 lg:gap-7 mt-[4px] lg:mt-[16px]">
+            <a href="https://app.assurify.app/" target="_blank" rel="noopener noreferrer">
               <Button>How it works</Button>
             </a>
-            <a
-              href='https://assurify.app/schedule-a-demo/'
-              target='_blank'
-              rel='noopener noreferrer'>
-              <Button variant='secondary-outline'>View Demo</Button>
+            <a href="https://assurify.app/schedule-a-demo/" target="_blank" rel="noopener noreferrer">
+              <Button variant="secondary-outline">View Demo</Button>
             </a>
           </div>
         </div>
       </div>
     </div>
-  )
-}
-export default Banner
+  );
+};
+export default Banner;
