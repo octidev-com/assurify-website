3bb16e398bca665f95fe20d6b23aea4653e66ff9
Mahmudul Haque Noor <mahmudulhaquenoor@gmail.com>
2025-05-16 16:27:28 +0600

    feat: theme variables add to the benefit component

--- src/component/Home/Benfits.jsx
+++ src/component/Home/Benfits.jsx
@@ -1,32 +1,32 @@
-import React from 'react'
-import roundBg from '../../assets/banner/round-bg.png'
-import benefit1 from '../../assets/benefits/benefit-1.png'
-import benefit2 from '../../assets/benefits/benefit-2.png'
-import benefit3 from '../../assets/benefits/benefit-3.png'
-import Container from '../Common/Container'
-import SectionTitle from '../Common/SectionTitle'
+import React from "react"
+import roundBg from "../../assets/banner/round-bg.png"
+import benefit1 from "../../assets/benefits/benefit-1.png"
+import benefit2 from "../../assets/benefits/benefit-2.png"
+import benefit3 from "../../assets/benefits/benefit-3.png"
+import Container from "../Common/Container"
+import SectionTitle from "../Common/SectionTitle"
 
 const Benefits = () => {
   const benefitsData = [
     {
       id: 1,
-      title: 'No Direct Cost to Sellers',
+      title: "No Direct Cost to Sellers",
       description:
-        'Offer premium shipping protection at no additional cost to you. Assurify adds value for customers while increasing your profits—without cutting into margins.',
+        "Offer premium shipping protection at no additional cost to you. Assurify adds value for customers while increasing your profits—without cutting into margins.",
       image: benefit1
     },
     {
       id: 2,
-      title: 'Flexible Integrations',
+      title: "Flexible Integrations",
       description:
-        'Assurify integrates seamlessly with your existing Shopify store—no technical hassle, no workflow disruptions.',
+        "Assurify integrates seamlessly with your existing Shopify store—no technical hassle, no workflow disruptions.",
       image: benefit2
     },
     {
       id: 3,
-      title: 'Superior Customer Service',
+      title: "Superior Customer Service",
       description:
-        'Get 24/7 support from a team that truly understands your business. From setup to claims, we’re here for you and your customers.',
+        "Get 24/7 support from a team that truly understands your business. From setup to claims, we’re here for you and your customers.",
       image: benefit3
     }
   ]
@@ -40,7 +40,7 @@ const Benefits = () => {
             src={roundBg}
             alt='Background decoration'
             className='w-[150vw] sm:w-[120vw] md:w-[100vw] lg:w-[75vw] max-w-none'
-            loading="lazy"
+            loading='lazy'
           />
         </div>
 
@@ -49,10 +49,10 @@ const Benefits = () => {
           {/* Left side of the main content (header) */}
 
           <SectionTitle
-            topText={'Benefits'}
-            middleText={'A Win-Win for Both Merchants and Customers'}
+            topText={"Benefits"}
+            middleText={"A Win-Win for Both Merchants and Customers"}
             bottomText={
-              'The perfect balance—customers get premium value, and merchants increase sales effortlessly!'
+              "The perfect balance—customers get premium value, and merchants increase sales effortlessly!"
             }
             isTextCentered={false}
           />
@@ -68,13 +68,13 @@ const Benefits = () => {
                     src={benefit.image}
                     alt={`benefit-${benefit.id}`}
                     className='w-[24px] md:w-[32px] h-[24px] md:h-[32px]'
-                    loading="lazy"
+                    loading='lazy'
                   />
                 </div>
-                <h3 className='text-[16px] md:text-[32px] font-normal text-[#f7f7f7] leading-[24px] md:leading-[48px]'>
+                <h3 className='text-h3-mobile lg:text-h3-desktop font-normal text-white leading-heading'>
                   {benefit.title}
                 </h3>
-                <p className='text-[12px]  md:text-[16px] font-normal leading-[21px]  md:leading-[30px] text-[#A6A6A6]'>
+                <p className='text-normal-paragraph-mobile lg:text-normal-paragraph-desktop font-normal leading-paragraph text-gray'>
                   {benefit.description}
                 </p>
               </div>
