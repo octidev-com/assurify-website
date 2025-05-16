4bf06caaae75b4f30fb9813576992a2cf3b6d2f0
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2025-05-16 13:23:13 +0600

    refactor: add theme variables

--- src/component/Common/SectionTitle.jsx
+++ src/component/Common/SectionTitle.jsx
@@ -1,7 +1,7 @@
-import leftSideHrDesktop from '../../assets/SectionTitle/left-side-hr-desktop.png'
-import leftSideHrMobile from '../../assets/SectionTitle/left-side-hr-mobile.png'
-import rightSideHrDesktop from '../../assets/SectionTitle/right-side-hr-desktop.png'
-import rightSideHrMobile from '../../assets/SectionTitle/right-side-hr-mobile.png'
+import leftSideHrDesktop from "../../assets/SectionTitle/left-side-hr-desktop.png";
+import leftSideHrMobile from "../../assets/SectionTitle/left-side-hr-mobile.png";
+import rightSideHrDesktop from "../../assets/SectionTitle/right-side-hr-desktop.png";
+import rightSideHrMobile from "../../assets/SectionTitle/right-side-hr-mobile.png";
 
 /**
  * A reusable and responsive section header component with multiple props
@@ -19,62 +19,46 @@ const SectionTitle = ({
   middleText,
   bottomText,
   isTextCentered = true,
-  useH1 = false // Default to false to maintain h2 behavior
+  useH1 = false, // Default to false to maintain h2 behavior
 }) => {
   return (
-    <div className='mx-auto flex flex-col items-center gap-2'>
+    <div className="mx-auto flex flex-col items-center gap-2">
       {topText && (
-        <div className='flex justify-center items-center gap-3'>
+        <div className={`w-full flex ${isTextCentered ? "justify-center" : "justify-start"} items-center gap-3`}>
           {/* LEFT HR FOR DESKTOP, HIDDEN ON MOBILE */}
-          <img
-            src={leftSideHrDesktop}
-            className='hidden lg:block'
-            alt='Left horizontal line'
-          />
+          <img src={leftSideHrDesktop} className="hidden lg:block" alt="Left horizontal line" />
           {/* LEFT HR FOR MOBILE, HIDDEN ON DESKTOP */}
-          <img
-            src={leftSideHrMobile}
-            className='block lg:hidden'
-            alt='Left horizontal line'
-          />
+          <img src={leftSideHrMobile} className="block lg:hidden" alt="Left horizontal line" />
 
           {/* TITLE TOP TEXT */}
-          <p className='text-[14px] text-center lg:text-xl font-normal leading-[21px] lg:leading-[30px]'>
-            {topText}
-          </p>
+          <p className="text-h5-mobile lg:text-h5-desktop text-center font-normal leading-paragraph">{topText}</p>
 
           {/* RIGHT HR FOR DESKTOP, HIDDEN ON MOBILE */}
-          <img
-            src={rightSideHrDesktop}
-            className='hidden lg:block'
-            alt='Right horizontal line'
-          />
+          <img src={rightSideHrDesktop} className="hidden lg:block" alt="Right horizontal line" />
           {/* RIGHT HR FOR MOBILE, HIDDEN ON DESKTOP */}
-          <img
-            src={rightSideHrMobile}
-            className='block lg:hidden'
-            alt='Right horizontal line'
-          />
+          <img src={rightSideHrMobile} className="block lg:hidden" alt="Right horizontal line" />
         </div>
       )}
       {middleText && (
         <div
-          className={`max-w-[778px] mx-auto text-[24px] lg:text-5xl font-semibold text-gradient leading-[36px] lg:leading-[72px] text-center md:${
-            isTextCentered ? 'text-center' : 'text-left'
-          }`}>
+          className={`max-w-[778px] mx-auto text-h1-mobile lg:text-h1-desktop font-semibold leading-paragraph text-gradient text-center md:${
+            isTextCentered ? "text-center" : "text-left"
+          }`}
+        >
           {useH1 ? <h1>{middleText}</h1> : <h2>{middleText}</h2>}
         </div>
       )}
       {bottomText && (
         <p
-          className={`max-w-[732px] mx-auto text-[#A6A6A6] text-[12px] lg:text-lg font-light leading-[18px] lg:leading-[27px] text-center md:${
-            isTextCentered ? 'text-center' : 'text-left'
-          }`}>
+          className={`max-w-[732px] text-large-paragraph-mobile lg:text-large-paragraph-desktop font-light leading-paragraph mx-auto text-gray
+           ${isTextCentered ? "text-center" : "text-left"}
+          }`}
+        >
           {bottomText}
         </p>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default SectionTitle
+export default SectionTitle;
